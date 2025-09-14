2ddac6bea146eb80ea25950bd5b76f66c2e7792f
Vitaly Bikeev <shum41k@yandex.ru>
2025-09-14 19:19:45 +0400

    исправления

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import pytest
-
+import logging
 from helps import Courier
 
+logger = logging.getLogger(__name__)
 
-# фикстура регистрации, авторизации и удаления курьера
 @pytest.fixture()
 def courier():
     courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
@@ -11,10 +11,9 @@ def courier():
     yield courier_create
     Courier().courier_subsequent_deletion(courier_login["id"])
 
-# фикстура регистрации, авторизации и удаления курьера
 @pytest.fixture()
 def courier_delete():
     courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    print(courier_create['data'])
+    # logger.info(courier_create['data'])  # если нужно логировать, иначе убрать print
     courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
     yield courier_login
